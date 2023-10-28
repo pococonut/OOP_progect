591b114f9ca9f3da8598d71d80a05d46702f82c4
Polina Zolotukhina <polinalinana030@gmail.com>
2023-10-28 21:26:09 +0300

    В функции сбора координат исправлена ошибка Ошибка возникала при попытке очищения поля ввода адресса, из-за чего собиралась неверная информация.

--- coordinates.py
+++ coordinates.py
@@ -28,15 +28,15 @@ def get_coordinates(json_addresses, url):
             number_coord_dict = {}
             for num in number_building:
                 if list(street_coord_dict.values()) and num in list(street_coord_dict.values())[0]:
-                    print('\nСОХРАНЕННЫЙ АДРЕС:', street, num)  # изменить проверку с улицы на дом
+                    print('\nСОХРАНЕННЫЙ АДРЕС:', street, num, '\n')  # изменить проверку с улицы на дом
                     continue
 
-                time.sleep(3)
                 search = browser.find_element(By.XPATH, "//input[@class='mapboxgl-ctrl-geocoder--input']")
-                search.send_keys(Keys.SHIFT + Keys.HOME + Keys.DELETE)
-                search.send_keys(f"Краснодарский Край, Краснодар {street} {num}")
+                search.clear()
+                time.sleep(1)
+                search.send_keys(f"Краснодарский Край, Краснодар {street} {num} ")
                 search.send_keys(Keys.ENTER)
-
+                time.sleep(2)
                 soup = BeautifulSoup(browser.page_source, 'lxml')
                 grid_coordinates = soup.find('div', class_='w-full mt12 txt-ms').find('div', class_='relative')
 
@@ -49,75 +49,6 @@ def get_coordinates(json_addresses, url):
                     print('Нет координат для адреса:', street, num)
                     coordinates = None
 
-                number_coord_dict[num] = coordinates
-            street_coord_dict[street] = number_coord_dict
-
-            print('\nstreet_coord_dict')
-            for k, v in street_coord_dict.items():
-                print(f'{k}\n{v}\n')
-
-            with open(f'files/coord_intermediate/coord_{key}.json', 'w') as outfile:
-                json.dump(street_coord_dict, outfile)
-
-        addresses_coord_dict[key] = street_coord_dict
-
-        with open('files/coordinates.json', 'w') as outfile:
-            json.dump(addresses_coord_dict, outfile)
-
-    return addresses_coord_dict
-
-
-def get_coordinates_old(json_addresses, url):
-    """
-    Функция для получения координат адреса.
-    :param json_addresses: Словарь с адресами.
-    :param url: Адрес сайта, с которого ведется парсинг.
-    :return:
-    """
-    browser = wd.Chrome()
-    browser.get(url)
-    browser.implicitly_wait(15)
-
-    addresses_coord_dict = get_dict("files/coordinates.json")
-
-    for key, value in json_addresses.items():
-        if key in addresses_coord_dict:
-            continue
-
-        street_coord_dict = get_dict(f"files/coord_intermediate/coord_{key}.json")
-
-        for street, number_building in value.items():
-            number_coord_dict = {}
-            for num in number_building:
-                if num in list(street_coord_dict.values())[0]:
-                    print('\nСОХРАНЕННЫЙ АДРЕС:', street, num)  # изменить проверку с улицы на дом
-                    continue
-
-                coordinates = []
-                time.sleep(2)
-                print(street, num)
-
-                search = browser.find_element(By.XPATH, "//input[@class='custom-input__input']")
-                search.send_keys(Keys.SHIFT + Keys.HOME + Keys.DELETE)
-                button_element = browser.find_element(By.XPATH, "//button[@class='demo-form__button accented xxx-big']")
-                search.send_keys(f"г. Краснодар, {street} {num}")
-
-                try:
-                    button_element.click()
-                except Exception as e:
-                    print(e)
-                    continue
-
-                soup = BeautifulSoup(browser.page_source, 'lxml')
-                grid_coordinates = soup.find_all('div', class_='grid-2-cols--withMobile')  # green-link-only-hover f-16
-                if grid_coordinates:
-                    grid_coordinates = grid_coordinates[0].find_all('span')[:5]
-                    for el in grid_coordinates:
-                        if not el.has_attr('class'):
-                            coordinates.append(el.text)
-                else:
-                    print('Нет координат для адреса:', street, num)
-                    coordinates = None
                 number_coord_dict[num] = coordinates
             street_coord_dict[street] = number_coord_dict
 
--- main.py
+++ main.py
@@ -11,8 +11,7 @@ url_buildings_info = 'https://аис.фрт.рф/myhouse'
 url_addresses = 'https://krasnodar.ginfo.ru/ulicy/'
 url_districts_v1 = 'https://krasnodar.kitabi.ru/map/street'
 url_districts_v2 = "https://youkarta.ru/krasnodarskij-kraj/krasnodar-23/"
-url_coordinates_v1 = "https://dadata.ru/product/geocode/"
-url_coordinates_v2 = 'https://docs.mapbox.com/playground/geocoding/'
+url_coordinates = 'https://docs.mapbox.com/playground/geocoding/'
 
 with open(r'files/addresses_v2.json', 'r') as openfile:
     j_object = json.load(openfile)
@@ -22,16 +21,15 @@ try:
     # addresses = get_addresses(url_addresses)
     # buildings_info = get_buildings_info(j_object, url_buildings_info)
     # districts = get_districts_v1(url_districts)
-    coordinates = get_coordinates(j_object, url_coordinates_v2)
+    coordinates = get_coordinates(j_object, url_coordinates)
 except Exception as e:
     print(e)
 
-"""s = 0
-for k, v in j_object.items():
+s = 0
+for k, v in j_object['Л'].items():
     print(k, ":", v)
     s += len(v)
-
-print(s)"""
+print(s)
 print("--- %s seconds ---" % ((time.time() - start_time)/60))
 
 #233 #289
