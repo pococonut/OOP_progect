78864fcc0bfb60b184f98c4ba66d3ab21efcb916
Polina Zolotukhina <polinalinana030@gmail.com>
2023-11-03 20:59:12 +0300

    Добавлен постраничный сбор ссылок в buildings_info.py

--- buildings_info.py
+++ buildings_info.py
@@ -1,13 +1,10 @@
 import time
 import json
-import os
 from bs4 import BeautifulSoup
 from selenium.webdriver import Keys
-
 from common_func import get_dict
 from selenium import webdriver as wd
 from selenium.webdriver.common.by import By
-from common_func import browser_connect
 
 
 def get_buildings_info(json_addresses, url):
@@ -37,77 +34,99 @@ def get_buildings_info(json_addresses, url):
         # перебираем улицы в множестве
         print(street_info_dict)
         for street in streets:
-            if street in street_info_dict:
-                print('СОХРАНЕННАЯ УЛИЦА:', street)
-                continue
+            if street == 'Улица Ленина':
+                if street in street_info_dict:
+                    print('СОХРАНЕННАЯ УЛИЦА:', street)
+                    continue
 
-            time.sleep(5)
-            browser.implicitly_wait(15)
-            # ищем информацию об улице
-
-            search = browser.find_element(By.XPATH, f'//input[@class="{search_class}"]')
-            search.send_keys(Keys.SHIFT + Keys.HOME + Keys.DELETE)
-
-            # w-100 ui-autocomplete-input
-            search.send_keys(f"край. Краснодарский, г. Краснодар, {street}")
-            button_element = browser.find_element(By.XPATH, f'//{button_teg}[@class="{button_class}"]') # green-button-text green-button
-            button_element.click()
-
-            search_class = "w-100 ui-autocomplete-input"
-            button_class = "green-button-text green-button-outline"
-            button_teg = 'input'
-
-            soup = BeautifulSoup(browser.page_source, 'lxml')
-            street_addresses = soup.find_all('a', class_='green-link-only-hover f-16') # green-link-only-hover f-16
-            # если информация не найдена пропускаем итерацию
-            if not street_addresses:
-                print('\nНет адресов для улицы:', street, '\n')
-                street_info_dict[street] = None
-                continue
-            # перебираем ссылки на адреса
-            build_info_dict = {}
-            for article in street_addresses:
-                time.sleep(2)
-                current_url = 'https://аис.фрт.рф' + article['href']
-                browser.get(current_url)
+                time.sleep(5)
                 browser.implicitly_wait(15)
+                # ищем информацию об улице
+
+                search = browser.find_element(By.XPATH, f'//input[@class="{search_class}"]')
+                search.send_keys(Keys.SHIFT + Keys.HOME + Keys.DELETE)
+
+                # w-100 ui-autocomplete-input
+                search.send_keys(f"край. Краснодарский, г. Краснодар, {street}")
+                button_element = browser.find_element(By.XPATH, f'//{button_teg}[@class="{button_class}"]')
+                button_element.click()
+
+                search_class = "w-100 ui-autocomplete-input"
+                button_class = "green-button-text green-button-outline"
+                button_teg = 'input'
+
                 soup = BeautifulSoup(browser.page_source, 'lxml')
-                num_building_info = soup.find('div', class_='house-description-address__title') # col-12 lg-bold-gilroy text-white mt-40 p-0
-                if not num_building_info:
-                    print('ERROR', num_building_info)
+
+                street_addresses = soup.find_all('a', class_='green-link-only-hover f-16')
+                pagination = soup.find('ul', class_='pagination fl mb-0')
+
+                if pagination:
+                    pages = pagination.find_all('a', class_='page-link')
+                    for i in range(2, int(pages[-2].text) + 1):
+                        link = browser.find_element(By.LINK_TEXT, str(i))
+
+                        # button_a = browser.find_element(By.XPATH, "//a[@aria-label='Next']")
+                        browser.execute_script("arguments[0].click();", link)
+                        browser.implicitly_wait(15)
+                        time.sleep(1)
+                        soup = BeautifulSoup(browser.page_source, 'lxml')
+                        street_addresses.extend(soup.find_all('a', class_='green-link-only-hover f-16'))
+
+                # если информация не найдена пропускаем итерацию
+                if not street_addresses:
+                    print('\nНет адресов для улицы:', street, '\n')
+                    street_info_dict[street] = None
                     continue
-                # получаем номер дома
-                num_building_info = num_building_info.text
-                print('Номер дома до обработки:', num_building_info)
-                symbol = ' д ' if ' д. ' not in num_building_info else ' д. '
-                try:
-                    if len(num_building_info.split(symbol)) >= 2:
-                        num_building = num_building_info.split(symbol)[1]
-                    else:
-                        num_building = num_building_info.split('д.')[1]
-                except Exception as e:
-                    print(e)
-                    continue
 
-                num_building = "".join([i for i in num_building if i.isdigit() or i.isalpha() or i == '/'])
-                print('Номер дома после обработки:', num_building, '\n')
-                # получаем информацию о здании
-                all_info = soup.find('div', class_='house-description-info')
-                div = all_info.find_all('div')
-                div_classes = ('text-secondary', 'f-16', 'fw-500')
-                inf_from_div = [d.text for d in div if d.has_attr('class') and d['class'][0] in div_classes]
-                dict_info_building = dict(zip(inf_from_div[::2], inf_from_div[1::2]))
-                build_info_dict[num_building] = dict_info_building
-            street_info_dict[street] = build_info_dict
+                # перебираем ссылки на адреса
+                build_info_dict = {}
+                for article in street_addresses:
+                    time.sleep(2)
+                    current_url = 'https://аис.фрт.рф' + article['href']
+                    browser.get(current_url)
+                    browser.implicitly_wait(15)
+                    soup = BeautifulSoup(browser.page_source, 'lxml')
+                    num_building = soup.find('div', class_='house-description-address__title')
+                    if not num_building:
+                        print('ERROR', num_building)
+                        continue
+                    # получаем номер дома
+                    num_building = num_building.text
 
-            search_class = 'col-12 py-3 ui-autocomplete-input'
-            button_class = 'col-12 find-button text-uppercase'
-            button_teg = 'button'
+                    not_allow = ['почтовое отделение', 'г. краснодар, х.',
+                                 'г. краснодар, п.', 'г краснодар, х', 'ст-ца']
+                    if [True for i in not_allow if i in num_building.lower()]:
+                        continue
 
-            print(f'street_info_dict:')
-            for k, v in street_info_dict.items():
-                print(f'{k}: {v}')
-            print()
+                    print('Номер дома до обработки:', num_building)
+                    symbol = ' д ' if ' д. ' not in num_building else ' д. '
+                    try:
+                        if len(num_building.split(symbol)) >= 2:
+                            num = num_building.split(symbol)[1]
+                        else:
+                            num = num_building.split('д.')[1]
+                    except Exception as e:
+                        print(e)
+                        continue
+
+                    num = "".join([i for i in num if i.isdigit() or i.isalpha() or i == '/'])
+                    print('Номер дома после обработки:', num, '\n')
+                    # получаем информацию о здании
+                    all_info = soup.find('div', class_='house-description-info')
+                    div = all_info.find_all('div')
+                    div_classes = ('text-secondary', 'f-16', 'fw-500')
+                    inf_from_div = [d.text for d in div if d.has_attr('class') and d['class'][0] in div_classes]
+                    dict_info_building = dict(zip(inf_from_div[::2], inf_from_div[1::2]))
+                    build_info_dict[num] = dict_info_building
+
+                street_info_dict[street] = build_info_dict
+                search_class = 'col-12 py-3 ui-autocomplete-input'
+                button_class = 'col-12 find-button text-uppercase'
+                button_teg = 'button'
+                print(f'street_info_dict:')
+                for k, v in street_info_dict.items():
+                    print(f'{k}: {v}')
+                print()
 
             with open(f'files/buildings_intermediate/buildings_info_{key}.json', 'w') as outfile:
                 json.dump(street_info_dict, outfile)
--- main.py
+++ main.py
@@ -16,20 +16,50 @@ url_coordinates = 'https://docs.mapbox.com/playground/geocoding/'
 with open(r'files/addresses_v2.json', 'r') as openfile:
     j_object = json.load(openfile)
 
+with open(r'files/buildings_info.json', 'r') as openfile:
+    b_object = json.load(openfile)
+
 try:
     # pass
     # addresses = get_addresses(url_addresses)
-    # buildings_info = get_buildings_info(j_object, url_buildings_info)
+    buildings_info = get_buildings_info(j_object, url_buildings_info)
     # districts = get_districts_v1(url_districts)
-    coordinates = get_coordinates(j_object, url_coordinates)
+    # coordinates = get_coordinates(j_object, url_coordinates)
 except Exception as e:
     print(e)
 
-s = 0
-for k, v in j_object['Л'].items():
-    print(k, ":", v)
-    s += len(v)
-print(s)
-print("--- %s seconds ---" % ((time.time() - start_time)/60))
+"""unic_num = []
+for k, v in j_object.items():
+    # print(f'-----------------------------{k}-----------------------------')
+    for s, n in v.items():
+        for i in n:
+            if i not in unic_num:
+                unic_num.append(i)
 
-#233 #289
+print()
+
+unic_num = {}
+unic_key = []
+
+not_null = 0
+null = 0
+for k, v in b_object.items():
+    print()
+    print(f'-----------------------------{k}-----------------------------')
+    print()
+    for s, n in v.items():
+        if n:
+            for i, j in n.items():
+                if i not in unic_num:
+                    unic_num[i] = s
+                # print(f'{s} {i}:   {j}')
+            not_null += 1
+        else:
+            null += 1
+print(not_null, null)
+for i, j in sorted(unic_num.items(), key=lambda x: (len(x[0]))):
+    if '(' in i:
+        print(f'{j} - {i}')"""
+
+
+print("--- %s seconds ---" % ((time.time() - start_time)/60))
